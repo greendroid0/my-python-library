aea7a9182647cdef0ed2a7c67be8edee789e4b50
Özge Eser Kolcu <ozge.eser35@gmail.com>
2024-08-28 00:16:55 +0200

    Add files via upload

---
+++ for-loop.py
@@ -0,0 +1,15 @@
+for i in range(1, 11):
+    print(i)
+
+name = "Hey yo "
+
+for letter in name:
+    print(letter)
+
+import time
+
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)
+
+print("Happy New Year!")
